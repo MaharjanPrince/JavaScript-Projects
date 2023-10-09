276adf85e22b2b83a32c5318e574577ca9d9f814
Prince Maharjan <maharjanxprince@gmail.com>
2023-10-09 20:40:24 +0545

    Final Commit : JavaScript was added and the site is working as a to do list it all works fine

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,21 +1,41 @@
-function addTask(){
-    const inputTask = document.getElementById('inputTask');
-    const listContainer = document.getElementById('listContainer');
 
-    if (inputTask.value !== ''){
-        const li = document.createElement('li');
-        li.textContent = inputTask.value;
+const inputTask = document.getElementById("input-Box");
+const listContainer = document.getElementById("listContainer");
 
 
-        const deleleButton = document.createElement('button');
-        deleleButton.textContent = 'Delete';
-        deleleButton.onclick = function(){
-            li.remove();
-        };
 
-        li.appendChild(deleleButton);
-        listContainer.appendChild(li);
-        inputTask.value = '';
+function addTask() {
+    if(inputTask.value === ''){
+        alert("You must write something")
     }
-    
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputTask.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputTask.value = "";
+    saveData()
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData;
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+
+}
+showTask();
